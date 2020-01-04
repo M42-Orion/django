245270dcd58a16f05344857e306f236be7b69300
python-codes <1821199691@qq.com>
2020-01-04 16:10:12 +0800

    django项目

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    import requests
+    import json
+    api_request=requests.get("https://api.github.com/users?since=0")
+    api = json.loads(api_request.content)
+    return render(request,"home.html",{'api':api})
